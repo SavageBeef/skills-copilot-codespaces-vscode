ae42aa80c284cfc8a1c731062583608b7e03b017
ProfessorBeef <143833290+ProfessorBeef@users.noreply.github.com>
2023-09-27 00:36:38 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,35 @@
+//  Create web server
+//  1. Create a web server using Node's http module
+//  2. Create a simple web server that returns the contents of the comments.json file
+//  3. Create a simple web server that returns the contents of the comments.json file
+//     when the user visits the /comments URL
+//  4. Create a simple web server that returns the contents of the comments.json file
+//     when the user visits the /comments URL and returns a 404 error for all other URLs
+//  5. Create a simple web server that returns the contents of the comments.json file
+//     when the user visits the /comments URL and returns a 404 error for all other URLs
+//     using the Express framework
+//  6. Create a simple web server that returns the contents of the comments.json file
+//     when the user visits the /comments URL and returns a 404 error for all other URLs
+//     using the Express framework and the path module
+//
+//  Execute: node comments.js
+//
+//  Exercise 1: Create a web server using Node's http module
+//  1. Create a web server using Node's http module
+//  2. Create a simple web server that returns the contents of the comments.json file
+//  3. Create a simple web server that returns the contents of the comments.json file
+//     when the user visits the /comments URL
+//  4. Create a simple web server that returns the contents of the comments.json file
+//     when the user visits the /comments URL and returns a 404 error for all other URLs
+//  5. Create a simple web server that returns the contents of the comments.json file
+//     when the user visits the /comments URL and returns a 404 error for all other URLs
+//     using the Express framework
+//  6. Create a simple web server that returns the contents of the comments.json file
+//     when the user visits the /comments URL and returns a 404 error for all other URLs
+//     using the Express framework and the path module
+//
+//  Execute: node comments.js
+//
+//  Exercise 2: Create a web server using Node's http module
+//  1. Create a web server using Node's http module
+//  2. Create a simple web server that returns the contents of the comments.json
